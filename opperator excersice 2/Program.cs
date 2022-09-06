005328b402800387d7e6ba3d56b8bed17b7d4833
Peter luque <Peterluque21@gmail.com>
2022-09-06 01:21:18 -0400

    initial commit

---
+++ opperator excersice 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //explict typing - 
+            string variableName = "Hello";
+
+            //inferred typing -
+            var varName = "Hello";
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mob = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {areaOfCircle}");
+
+        }
+    }
+}     
+
+
